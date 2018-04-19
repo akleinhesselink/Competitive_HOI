99040340675e92099bc520cfc5c47414e0d5194f
Andy Kleinhesselink <andy.kleinhesselink@gmail.com>
2018-04-19 12:39:48 -0700

    Fix indentation and renumber form 3 as form 2

--- code/fit_pairwise_nls.R
+++ code/fit_pairwise_nls.R
@@ -6,28 +6,18 @@ source('code/figure_pars.R')
 data_file <- 'data/mechanistic_sim_bicultures.rds'
 
 basic <- 'y ~ lambda*(1 + alpha.[1]*N1 + alpha.[2]*N2 + alpha.[3]*N3)^tau. '
-basic2 <- 'y ~ (1 + alpha.[1]*N1 + alpha.[2]*N2 + alpha.[3]*N3 + alpha.[4]*I(N1^2) + alpha.[5]*I(N2^2) + alpha.[6]*I(N3^2))^tau. '
 
-type3 <- 'y ~ lambda*(1 + N1^(alpha.[1]) + N2^(alpha.[2]) + N3^(alpha.[3]))^(tau.)'
-
-type4 <- 'y ~ 1 + (gamma.[1]*N1)^(alpha.[1]) + (gamma.[2]*N2)^(alpha.[2]) + (gamma.[3]*N3)^(alpha.[3])'
+type2 <- 'y ~ lambda*(1 + N1^(alpha.[1]) + N2^(alpha.[2]) + N3^(alpha.[3]))^(tau.)'
 
 init_vals1 <- list(tau. = -1, alpha. = rep(1,3))
-init_vals2 <- list(tau. = 1.5, alpha. = c(rep(1,3), rep(0,3)))
 
-init_vals3 <- list(tau. = -1, alpha. = c(rep(3,3)))
-init_vals4 <- list(gamma. = rep(1,3), alpha. = c(rep(1,3)))
+init_vals2 <- list(tau. = -1, alpha. = c(rep(3,3)))
 
 lower1 <- c(-1.5, rep(0, 3))
 upper1 <- c(-0.1, rep(100,3))
-lower2 <- c(0.1, c(rep(0, 3), rep(-0.0001,3)))
-upper2 <- c(1.5, c(rep(100, 3), rep(2,3)))
 
-lower3 <- c(-1.2, rep(0.0001, 3))
-upper3 <- c(0.1, rep(5, 3))
-
-lower4 <- c(rep(0, 3), rep(1e-5, 3))
-upper4 <- c(rep(2, 3), rep(2.1, 3))
+lower2 <- c(-1.2, rep(0.0001, 3))
+upper2 <- c(0.1, rep(5, 3))
 
 mydat <- readRDS(data_file)
 
@@ -39,17 +29,24 @@ mydat <-
 
 mydat <- split(mydat , mydat$focal)
 
-fits1 <- lapply(mydat, FUN = fit_nls, form = basic, init_vals = init_vals1, lower = lower1, upper = upper1, algorithm = 'port')
+fits1 <- lapply(mydat, 
+                FUN = fit_nls, 
+                form = basic, 
+                init_vals = init_vals1, 
+                lower = lower1, 
+                upper = upper1, 
+                algorithm = 'port')
 
-# fits2 <- lapply(mydat, FUN = fit_nls, form = basic2, init_vals = init_vals2, lower = lower2, upper = upper2, algorithm = 'port')
-# 
-fits3 <- lapply(mydat, FUN = fit_nls, form = type3, init_vals = init_vals3, lower = lower3, upper = upper3, algorithm = 'port')
-
-# 
-# fits4 <- lapply(mydat, FUN = fit_nls, form = type4, init_vals = init_vals4, lower = lower4, upper = upper4, algorithm = 'port')
+fits2 <- lapply(mydat, 
+                FUN = fit_nls, 
+                form = type2, 
+                init_vals = init_vals2, 
+                lower = lower2, 
+                upper = upper2, 
+                algorithm = 'port')
 
 mydat <- mapply(fits1, mydat, FUN = function(x, y) {y$pred_1 <- predict( x, y); return(y) }, SIMPLIFY = F )
-mydat <- mapply(fits3, mydat, FUN = function(x, y) {y$pred_2 <- predict( x, y); return(y) }, SIMPLIFY = F )
+mydat <- mapply(fits2, mydat, FUN = function(x, y) {y$pred_2 <- predict( x, y); return(y) }, SIMPLIFY = F )
 
 mydat <- do.call(rbind, mydat)
 
