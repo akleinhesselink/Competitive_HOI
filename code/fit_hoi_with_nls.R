06a3267388d1c656e6ee91be8e3235006d4c5dc8
andy <andy.kleinhesselink@gmail.com>
2019-08-08 17:10:55 -0700

    Add new fits and plotting script

--- code/fit_hoi_with_nls.R
+++ code/fit_hoi_with_nls.R
@@ -5,14 +5,15 @@ library(tidyverse)
 source('code/plotting_parameters.R')
 source('code/simulation_functions.R')
 source('code/phenomenological_models.R')
-
 load( 'output/processed_results.rda')
 
 fit4 <- fit3 <- fit2 <- fit1 <- list()
 
 for( i in 1:3 ) { 
   
-  temp_data <- sim_results %>% filter( n_comp < 2, species == !!paste0('Y', i))
+  temp_data <- 
+    sim_results %>% 
+    filter( n_comp < 2, species == !!paste0('Y', i))
   
   fit1[[i]] <- nls( 
     log(1/y) ~ log(model1(B1, B2, B3, parms = list(lambda = lambda, alpha = alpha, tau = tau))), 
@@ -39,7 +40,7 @@ for( i in 1:3 ) {
     algorithm = 'port'
   )
   fit4[[i]] <- nls(
-    log(1/y) ~ log(model3(B1, B2, B3, parms = list(lambda = lambda, alpha = alpha, tau = tau))), 
+    log(1/y) ~ log(model4(B1, B2, B3, parms = list(lambda = lambda, alpha = alpha, tau = tau))), 
     data = temp_data, 
     start = inits4, 
     lower = lowers4, 
@@ -65,48 +66,36 @@ fit4[[1]]
 fit4[[2]]
 fit4[[3]]
 
-# filter out two competitor runs where one 
-# competitor is same as the focal species. 
-temp_data <- 
-  sim_results %>% 
-  filter( n_comp < 3, (n_comp < 2) | ( get(paste0('B', str_extract(species, '\\d') )) < 1 )  )
-
 fixed_pars3 <- lapply( fit3, get_fixed_pars)
-
-hoi_init <- list( c(beta = 0.1, eta = 0.2), c( beta = 0.1, eta = 0.2), c( beta = -0.1, eta = 0.2))
-hoi_lower <- list(c(0,0.1), c(0,0.1), c(-2,0.1))
-hoi_upper <- list(c(2, 2), c(2, 2), c(-0.1, 2))
-
-hoi_init2 <- lapply ( hoi_init, function(x) x[ 'beta' ] )
-hoi_lower2 <- lapply ( hoi_lower, function(x) x[1] )
-hoi_upper2 <- lapply ( hoi_upper, function(x) x[1 ] )
-
 
 
 HOI_type1_fit <- HOI_type2_fit <- list()
-
 for( i in 1:3) { 
   
+  temp_data <- 
+    sim_results %>% 
+    filter( n_comp < 3, species == !!paste0('Y', i))
+  
   HOI_type1_fit[[i]] <- nls( 
-    log( 1/y) ~  log( model_HOI_type_1(B1, B2, B3, h, beta = beta, eta = eta, fixed_parms = fixed_pars3[[i]] )) , 
-    data = temp_data %>% filter( species == !!paste0( 'Y', i) & HOI == 1), 
+    log( 1/y) ~  log( model_HOI_type_1(B1, B2, B3, beta = beta, eta = eta, fixed_parms = fixed_pars3[[i]] )) , 
+    data = temp_data,
     start = hoi_init[[i]], 
-    lower = hoi_lower[[i]], 
-    upper = hoi_upper[[i]], 
-    algorithm = 'port', 
-    control = list(maxiter = 1000, minFactor = 1/100000 )
+    lower = unlist( hoi_lower[[i]]), 
+    upper = unlist( hoi_upper[[i]]), 
+    algorithm = 'port'
   )
   
   HOI_type2_fit[[i]] <- 
     nls( 
-      log( 1/y) ~  log( model_HOI_type_2[[i]](B1, B2, B3, beta = beta, fixed_parms = fixed_pars3[[i]] )) , 
-      data = temp_data %>% filter( species == !!paste0( 'Y', i) & HOI == 1), 
-      start = hoi_init2[[i]], 
-      lower = hoi_lower2[[i]], 
-      upper = hoi_upper2[[i]], 
+      log( 1/y) ~  log( model_HOI_type_2(B1, B2, B3, beta = beta, fixed_parms = fixed_pars3[[i]] )) , 
+      data = temp_data,
+      start = hoi_init[[i]][1], 
+      lower = hoi_lower[[i]][1:3], 
+      upper = hoi_upper[[i]][1:3], 
       algorithm = 'port', 
       control = list(maxiter = 1000, minFactor = 1/100000 )
     )
+  
 }
 
 HOI_type1_fit[[1]]
@@ -117,29 +106,28 @@ HOI_type2_fit[[1]]
 HOI_type2_fit[[2]]
 HOI_type2_fit[[3]]
 
-pgrid$m1 <- NA
-pgrid$m2 <- NA
-pgrid$m3 <- NA
-pgrid$m4 <- NA
-pgrid$mHOI1 <- NA
-pgrid$mHOI2 <- NA
+predicted <- pgrid
+
+predicted$m1 <- NA
+predicted$m2 <- NA
+predicted$m3 <- NA
+predicted$m4 <- NA
+predicted$mHOI1 <- NA
+predicted$mHOI2 <- NA
 
 for( i in 1:3 ) { 
   focal <- paste0('Y', i)
-  pgrid$m1[pgrid$species == focal] <- 1/(exp( predict( fit1[[i]], newdata = pgrid[pgrid$species == focal, ]) ))
-  pgrid$m2[pgrid$species == focal] <- 1/(exp( predict( fit2[[i]], newdata = pgrid[pgrid$species == focal, ]) ))
-  pgrid$m3[pgrid$species == focal] <- 1/(exp( predict( fit3[[i]], newdata = pgrid[pgrid$species == focal, ]) ))
-  pgrid$m4[pgrid$species == focal] <- 1/(exp( predict( fit4[[i]], newdata = pgrid[pgrid$species == focal, ]) ))
+  predicted$m1[predicted$species == focal] <- 1/(exp( predict( fit1[[i]], newdata = predicted[predicted$species == focal, ]) ))
+  predicted$m2[predicted$species == focal] <- 1/(exp( predict( fit2[[i]], newdata = predicted[predicted$species == focal, ]) ))
+  predicted$m3[predicted$species == focal] <- 1/(exp( predict( fit3[[i]], newdata = predicted[predicted$species == focal, ]) ))
+  predicted$m4[predicted$species == focal] <- 1/(exp( predict( fit4[[i]], newdata = predicted[predicted$species == focal, ]) ))
   
-  pgrid$mHOI1[pgrid$species == focal] <- 1/(exp( predict( HOI_type1_fit[[i]], newdata = pgrid[pgrid$species == focal, ]) ))
-  pgrid$mHOI2[pgrid$species == focal] <- 1/(exp( predict( HOI_type2_fit[[i]], newdata = pgrid[pgrid$species == focal, ]) ))
+  predicted$mHOI1[predicted$species == focal] <- 1/(exp( predict( HOI_type1_fit[[i]], newdata = predicted[predicted$species == focal, ]) ))
+  predicted$mHOI2[predicted$species == focal] <- 1/(exp( predict( HOI_type2_fit[[i]], newdata = predicted[predicted$species == focal, ]) ))
 }
 
-
-
 save( 
-  temp_data, 
-  pgrid,
+  predicted,
   fit1, 
   fit2, 
   fit3, 
--- code/phenomenological_models.R
+++ code/phenomenological_models.R
@@ -16,80 +16,76 @@ uppers2 <- uppers1 <- c( uppers1[1:4], tau = 5)
 # simple pairwise models 
 model1 <- function(B1, B2, B3, parms ){
   with(parms, { 
-    (1/lambda)*(1 + B1*alpha[1] + B2*alpha[2] + B3*alpha[3] )^tau
+    t1 <- (B1*alpha[1]) 
+    t2 <- (B2*alpha[2])
+    t3 <- (B3*alpha[3])
+    
+    (1/lambda)*(1 + t1 + t2 + t3 )^tau
   })
 }
 
 model2 <- function(B1, B2, B3, parms ){
   # multiply separate species functions  
   with(parms, { 
-    (1/lambda)*((1 + B1*alpha[1])*(1 + B2*alpha[2])*(1 + B3*alpha[3]))^tau
+    t1 <- (B1*alpha[1]) 
+    t2 <- (B2*alpha[2])
+    t3 <- (B3*alpha[3])
+    
+    (1/lambda)*((1 + t1)*(1 + t2)*(1 + t3))^tau
   })
 }
 
 model3 <- function(B1, B2, B3, parms ){
   with(parms, { 
-    (1/lambda)*(1 + (B1*alpha[1])^tau[1] + (B2*alpha[2])^tau[2] + (B3*alpha[3])^tau[3] )
+    t1 <- (B1*alpha[1])^tau[1] 
+    t2 <- (B2*alpha[2])^tau[2]
+    t3 <- (B3*alpha[3])^tau[3]
+    
+    (1/lambda)*(1 + t1 + t2 + t3 )
   })
 }
 
 model4 <- function(B1, B2, B3, parms ){
   with(parms, { 
-    (1/lambda)*((1 + (B1*alpha[1])^tau[1])*(1 + (B2*alpha[2])^tau[2])*(1 + (B3*alpha[3])^tau[3]))^(-1)
+    t1 <- (B1*alpha[1])^tau[1] 
+    t2 <- (B2*alpha[2])^tau[2]
+    t3 <- (B3*alpha[3])^tau[3]
+    
+    (1/lambda)*((1 + t1)*(1 + t2)*(1 + t3))
   })
 }
 
 # HOI models 
 
-model_HOI_type_1 <- function(B1, B2, B3, h, beta, eta, fixed_parms ){
-  # formula is inverted 
-  # fixed parms are from the pairwise fit 
-  
-  HOI <- beta*(h)^eta
-
-  with(fixed_parms, {  
-    (1/lambda)*(1 + (B1*alpha[1])^tau[1] + (B2*alpha[2])^tau[2] + (B3*alpha[3])^tau[3] + HOI)
-  })
-}
-
-model_HOI_1 <- function(B1, B2, B3, beta, fixed_parms ){
+model_HOI_type_1 <- function(B1, B2, B3, beta, eta, fixed_parms ){
   # formula is inverted 
   # fixed parms are from the pairwise fit 
   
   with(fixed_parms, {  
-    HOI <- beta*( (B2*alpha[2])^tau[2]*(B3*alpha[3])^tau[3] )
+    t1 <- (B1*alpha[1])^tau[1] 
+    t2 <- (B2*alpha[2])^tau[2]
+    t3 <- (B3*alpha[3])^tau[3]
     
-    (1/lambda)*(1 + (B1*alpha[1])^tau[1] + (B2*alpha[2])^tau[2] + (B3*alpha[3])^tau[3] + HOI)
+    HOI <- beta[1]*(B1*B2)^eta[1] + beta[2]*(B1*B3)^eta[2] + beta[3]*(B2*B3)^eta[3]
+    
+    (1/lambda)*(1 + t1 + t2 + t3 + HOI)
   })
 }
 
-model_HOI_2 <- function(B1, B2, B3, beta, fixed_parms ){
+model_HOI_type_2 <- function(B1, B2, B3, beta, fixed_parms ){
   # formula is inverted 
   # fixed parms are from the pairwise fit 
   
   with(fixed_parms, {  
+    t1 <- (B1*alpha[1])^tau[1] 
+    t2 <- (B2*alpha[2])^tau[2]
+    t3 <- (B3*alpha[3])^tau[3]
     
-    HOI <- beta*( (B1*alpha[1])^tau[1]*(B3*alpha[3])^tau[3] )
-    (1/lambda)*(1 + (B1*alpha[1])^tau[1] + (B2*alpha[2])^tau[2] + (B3*alpha[3])^tau[3] + HOI)
+    HOI <- beta[1]*(t1*t2) + beta[2]*(t1*t3) + beta[3]*(t2*t3)
     
+    (1/lambda)*(1 + t1 + t2 + t3 + HOI)
   })
 }
-
-
-
-model_HOI_3 <- function(B1, B2, B3, beta, fixed_parms ){
-  # formula is inverted 
-  # fixed parms are from the pairwise fit 
-  
-  with(fixed_parms, {  
-    
-    HOI <- beta*( (B1*alpha[1])^tau[1]*(B2*alpha[2])^tau[2] )
-    
-    (1/lambda)*(1 + (B1*alpha[1])^tau[1] + (B2*alpha[2])^tau[2] + (B3*alpha[3])^tau[3] + HOI)
-  })
-}
-
-model_HOI_type_2 <- list( model_HOI_1, model_HOI_2, model_HOI_3 )
 
 get_fixed_pars <- function(myfit ){ 
   
@@ -101,5 +97,26 @@ get_fixed_pars <- function(myfit ){
 }
 
 
+beta_init <- matrix( rep(0.1, 9), 3, 3)
+beta_init <- beta_init*matrix( c(rep(1, 6), -1, 1,1), 3,3, byrow = T )
+
+eta_init <- matrix( rep(0.5, 9), 3,3)
+beta_lower <- (beta_init - abs(beta_init))*10
+beta_upper <- (beta_init + abs(beta_init))*10
+
+eta_lower <- eta_init*0.1
+eta_upper <- eta_init*2
+
+hoi_init <- list(list(beta = beta_init[1, ], eta = eta_init[1, ]),
+                 list(beta = beta_init[2, ], eta = eta_init[2, ]),
+                 list(beta = beta_init[3, ], eta = eta_init[3, ]))
+
+hoi_upper <- list( c(beta_upper[1,], eta_upper[1, ]), 
+                   c(beta_upper[2,], eta_upper[2, ]), 
+                   c(beta_upper[3,], eta_upper[3, ]))
+
+hoi_lower <- list( c(beta_lower[1,], eta_lower[1, ]), 
+                   c(beta_lower[2,], eta_lower[2, ]), 
+                   c(beta_lower[3,], eta_lower[3, ]))
 
 
